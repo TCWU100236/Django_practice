11f3e6e6b8acdbe71ca2c71be26444cc999145c8
hola7671 <hola7671@gmail.com>
2025-04-22 22:32:11 +0800

    update

--- dj4ch08/dj4ch08/mysite/views.py
+++ dj4ch08/dj4ch08/mysite/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from mysite import models
 
 # Create your views here.
 def index(request):
+    posts = models.Post.objects.filter(enabled = True).order_by("-pub_time")[:30]
+    moods = models.Mood.objects.all()
+    try:
+        user_id = request.GET["user_id"]
+        user_pass = request.GET["user_pass"]
+        user_post = request.GET["user_post"]
+        user_mood = request.GET["mood"]
+    except:
+        user_id = None
+        message = "如果要張貼訊息，則每一個欄位都要填..."
+
+    if user_id != None:
+        mood = models.Mood.objects.get(status=user_mood)
+        post = models.Post(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
+        post.save()
+        message = f"成功儲存!請記得你的編輯密碼[{user_pass}]!，訊息經審查後才會顯示。"
     return render(request, "index.html", locals())
