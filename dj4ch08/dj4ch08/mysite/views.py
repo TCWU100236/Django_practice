02ab1789009bf3c6ebd23f127fca322053144221
TCWU100236 <hola7671@gmail.com>
2025-04-24 21:55:53 +0800

    update

--- dj4ch08/dj4ch08/mysite/views.py
+++ dj4ch08/dj4ch08/mysite/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from mysite import models
+from django.core.mail import EmailMessage
+from django.shortcuts import render, redirect
+from mysite import models, forms
+import json
+import urllib
+from django.conf import settings
 
 # Create your views here.
 def index(request):
@@ -20,3 +24,97 @@ def index(request):
         post.save()
         message = f"成功儲存!請記得你的編輯密碼[{user_pass}]!，訊息經審查後才會顯示。"
     return render(request, "index.html", locals())
+
+def delpost(request, pid=None, del_pass=None):
+    if del_pass and pid:
+        try:
+            post = models.Post.objects.get(id=pid)
+            if post.del_pass == del_pass:
+                post.delete()
+        except:
+            pass
+    return redirect("/")
+
+def listing(request):
+    posts = models.Post.objects.filter(enabled=True).order_by("-pub_time")[:150]
+    moods = models.Mood.objects.all()
+    return render(request, "listing.html", locals())
+
+def posting(request):
+    moods = models.Mood.objects.all()
+    message = "如果要張貼訊息，則每一個欄位都要填..."
+
+    if request.method == "POST":
+        user_id = request.POST.get("user_id")
+        user_pass = request.POST.get("user_pass")
+        user_post = request.POST.get("user_post")
+        user_mood = request.POST.get("mood")
+        if user_id != None:
+            mood = models.Mood.objects.get(status=user_mood)
+            post = models.Post(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post, enabled=True)
+            post.save()
+            return redirect("/list/")
+
+    return render(request, "posting.html", locals())
+
+def contact(request):
+    if request.method == "POST":
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            message = "感謝您的來信，我們會儘速處理您的寶貴意見。"
+            user_name = form.cleaned_data["user_name"]
+            user_city = form.cleaned_data["user_city"]
+            user_school = form.cleaned_data["user_school"]
+            user_email = form.cleaned_data["user_email"]
+            user_message = form.cleaned_data["user_message"]
+
+# ====================================== 寄信功能 ======================================
+            mail_body = u'''
+網友姓名：{}
+居住城市：{}
+是否在學：{}
+電子郵件：{}
+反應意見：如下
+{}'''.format(user_name, user_city, user_school, user_email, user_message)
+
+            email = EmailMessage(   '來自【不吐不快】網站的網友意見', 
+                                    mail_body, 
+                                    user_email,
+                                    ['r76121243@gs.ncku.edu.tw'])
+            email.send()
+# ======================================================================================
+
+        else:
+            message = "請檢查您輸入的資訊是否正確!"
+    else:
+        form = forms.ContactForm()
+    return render(request, "contact.html", locals())
+
+def post2db(request):
+    if request.method == "POST":
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            data = urllib.parse.urlencode(values).encode()
+            req =  urllib.request.Request(url, data=data)
+            response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode())
+            if result['success']:
+                post_form.save()
+                return redirect('/list/')
+
+            # CAPTCHA 驗證
+            # message = "您的訊息已儲存，要等管理者啟用後才看得到喔。"
+            # post_form.save()
+            # return redirect("/list/")
+        else:
+            message = "如果要張貼訊息，則每一個欄位都要填..."
+    else:
+        post_form = forms.PostForm()
+        message = "如果要張貼訊息，則每一個欄位都要填..."
+    return render(request, "post2db.html", locals())
